97b6978e34bc0c8f02c358d4dbc395da7d3c6b41
Thuany Ramberger <thuanyramberger@Thuanys-MacBook-Pro.local>
2025-10-05 20:56:30 +0200

    set basepath correctly depending on gh/localhsot

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let count = 0;
 let increment = 1;
-let score = document.getElementById('score');
+let score = document.getElementById("score");
 let cookie = document.getElementById("cookie");
 let plus4Chocolate = document.getElementById("plus4Chocolate");
 let plus10Swedish = document.getElementById("plus10Swedish");
@@ -8,73 +8,68 @@ let plus20Xmas = document.getElementById("plus20Xmas");
 let milkLevel = document.getElementById("milkLevel");
 let chocolateChip = document.getElementById("chocolateChip");
 let milk = document.getElementById("milk");
-let milkAmount = 0
+let milkAmount = 0;
 
-
-
-cookie.addEventListener('click', function() {
+cookie.addEventListener("click", function () {
   increaseScore();
   changeScore();
   checkButtons();
   checkWin();
 });
 
-plus4Chocolate.addEventListener('click', function() {
-  if (count < 20){
-    alert("Don't be so hungry, you don't have enough points!")
+plus4Chocolate.addEventListener("click", function () {
+  if (count < 20) {
+    alert("Don't be so hungry, you don't have enough points!");
     return;
   }
   increment = 4;
-  chocolateChip.src = "/img/chocolate.png";
+  chocolateChip.src = "img/chocolate.png";
   count -= 20;
   changeScore();
-})
+});
 
-plus10Swedish.addEventListener('click', function() {
-  if (count < 40){
-    alert("Don't be so hungry, you don't have enough points!")
+plus10Swedish.addEventListener("click", function () {
+  if (count < 40) {
+    alert("Don't be so hungry, you don't have enough points!");
     return;
   }
   increment = 10;
-  chocolateChip.src = "/img/swedish.png";
+  chocolateChip.src = "img/swedish.png";
   count -= 40;
   changeScore();
-})
+});
 
-plus20Xmas.addEventListener('click', function() {
-  if (count < 80){
-    alert("Don't be so hungry, you don't have enough points!")
+plus20Xmas.addEventListener("click", function () {
+  if (count < 80) {
+    alert("Don't be so hungry, you don't have enough points!");
     return;
   }
   increment = 20;
-  chocolateChip.src = "/img/xmas.png";
+  chocolateChip.src = "img/xmas.png";
   count -= 80;
   changeScore();
-})
+});
 
-milkLevel.addEventListener('click', addMilk);
+milkLevel.addEventListener("click", addMilk);
 
-
-
-
-function addMilk () {
- if (count < 500) {
-   alert("Don't be so thirsty, you don't have enough points!")
-   return;
- }
+function addMilk() {
+  if (count < 500) {
+    alert("Don't be so thirsty, you don't have enough points!");
+    return;
+  }
   milkAmount++;
- if (milkAmount > 3) {
-   return;
- }
- milk.src = "/img/milk" + milkAmount + ".png";
- count -=50;
- changeScore();
+  if (milkAmount > 3) {
+    return;
+  }
+  milk.src = "img/milk" + milkAmount + ".png";
+  count -= 50;
+  changeScore();
 }
 
 function checkButtons() {
-  if (count > 790){
+  if (count > 790) {
     milkLevel.style.display = "block";
-  } else if (count > 490){
+  } else if (count > 490) {
     plus20Xmas.style.display = "block";
   } else if (count > 109) {
     plus10Swedish.style.display = "block";
@@ -91,8 +86,10 @@ function increaseScore() {
   count = count + increment;
 }
 
-function checkWin () {
+function checkWin() {
   if (count >= 1000) {
-    alert("1000 points - SWEET VICTORY! Your cookie stash is overflowing, your milk is perfectly poured - Your tummy (and heart) are full!")
+    alert(
+      "1000 points - SWEET VICTORY! Your cookie stash is overflowing, your milk is perfectly poured - Your tummy (and heart) are full!"
+    );
   }
 }
